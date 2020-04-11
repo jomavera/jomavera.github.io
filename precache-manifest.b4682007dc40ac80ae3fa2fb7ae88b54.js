7b86a13f72523334307558adb006b288168c4292
jomavera <josemanuel.vera.aray@gmail.com>
2020-04-10 23:12:38 -0400

    Updates

---
+++ precache-manifest.b4682007dc40ac80ae3fa2fb7ae88b54.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "966604027c0057389fb47603281f6677",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a2742da4e8a8d7536566",
+    "url": "/static/css/main.eebd6e03.chunk.css"
+  },
+  {
+    "revision": "d395de60c09b70cfaa2c",
+    "url": "/static/js/2.0a35a8cc.chunk.js"
+  },
+  {
+    "revision": "f0ab5801985173a52868f196793396c8",
+    "url": "/static/js/2.0a35a8cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2742da4e8a8d7536566",
+    "url": "/static/js/main.98d2cecf.chunk.js"
+  },
+  {
+    "revision": "020834944cab8ebe9408",
+    "url": "/static/js/runtime-main.99a7a007.js"
+  },
+  {
+    "revision": "c78e1161c91a81b3c7c0077abc876dff",
+    "url": "/static/media/bg_img_3.c78e1161.jpg"
+  },
+  {
+    "revision": "d1eca653d914ef7b69ef5b759d8666ae",
+    "url": "/static/media/init_problem.d1eca653.png"
+  },
+  {
+    "revision": "5b9f2e971c20cbe5808213a247dd34cc",
+    "url": "/static/media/problem.5b9f2e97.png"
+  },
+  {
+    "revision": "c85ad71a6b5e399c9522dbd981897830",
+    "url": "/static/media/profile_img.c85ad71a.jpg"
+  },
+  {
+    "revision": "bdc62726991f419d9e1e1a2a99f71689",
+    "url": "/static/media/results.bdc62726.png"
+  }
+]);
