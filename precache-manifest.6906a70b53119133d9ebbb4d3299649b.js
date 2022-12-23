95587a818bbc3416fc490ebbb078f7ad3fe85bc8
jomavera <josemanuel.vera.aray@gmail.com>
2022-12-23 14:50:45 -0300

    Manual update

---
+++ precache-manifest.6906a70b53119133d9ebbb4d3299649b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "404f3340c5403c96e27b90fc4a104af8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "519b59ede887188bc799",
+    "url": "/static/css/2.711045af.chunk.css"
+  },
+  {
+    "revision": "98b4dfafef38e0ce0b7c",
+    "url": "/static/css/main.307d5828.chunk.css"
+  },
+  {
+    "revision": "519b59ede887188bc799",
+    "url": "/static/js/2.13423074.chunk.js"
+  },
+  {
+    "revision": "078031341a08192a3c7d6fc85e4e486a",
+    "url": "/static/js/2.13423074.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98b4dfafef38e0ce0b7c",
+    "url": "/static/js/main.7731bf25.chunk.js"
+  },
+  {
+    "revision": "020834944cab8ebe9408",
+    "url": "/static/js/runtime-main.99a7a007.js"
+  },
+  {
+    "revision": "10213859e322bbe9b06e57a9445ed3bc",
+    "url": "/static/media/bg_img_3.10213859.jpg"
+  },
+  {
+    "revision": "7d06d4c6ac51ce14827d4e93eb0b9fc8",
+    "url": "/static/media/ecuacovid.7d06d4c6.PNG"
+  },
+  {
+    "revision": "fcb47d6441604d1dd41306f68aaa46fe",
+    "url": "/static/media/interior_point.fcb47d64.md"
+  },
+  {
+    "revision": "196014c0f798b359c0af1605b6b93e7b",
+    "url": "/static/media/photo_2.196014c0.jpg"
+  },
+  {
+    "revision": "4e114318ec9aa6bb314347a04228c38d",
+    "url": "/static/media/profile_img.4e114318.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4c515e029ddc04a497457e3b880212a8.js"
+  "/precache-manifest.6906a70b53119133d9ebbb4d3299649b.js"
 );
 
 self.addEventListener('message', (event) => {
